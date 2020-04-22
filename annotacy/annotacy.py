b40c3c461a86c985888f30edee524682d4a8bb00
gandersen101 <30376815+gandersen101@users.noreply.github.com>
2020-04-22 17:25:13 -0500

    Moving most arguments from class to methods

--- annotacy/annotacy.py
+++ annotacy/annotacy.py
@@ -1,13 +1,12 @@
 import re
-from heapq import nlargest
-from itertools import chain
 import string
 import operator
-from operator import itemgetter, truth
+from itertools import chain
 from functools import partial
+from heapq import nlargest
 import spacy
-from spacy.tokens import Span
 from fuzzywuzzy import fuzz
+from spacy.tokens import Span
 
 
 def map_chars_to_tokens(doc):
@@ -52,86 +51,130 @@ class RegexMatcher:
 class FuzzySearch:
     def __init__(
         self,
-        nlp,
-        fuzzy_alg=fuzz.ratio,
-        min_ratio=70,
-        case_sensitive=False,
+        nlp=spacy.blank("en"),
         left_ignores=("space", "punct", "stop"),
         right_ignores=("space", "punct", "stop"),
     ):
         self.nlp = nlp
-        self.fuzzy_alg = fuzzy_alg
-        self.min_ratio = min_ratio
-        self.case_sensitive = case_sensitive
         self.left_ignores = left_ignores
         self.right_ignores = right_ignores
         self.left_ignore_rules = {
-            "space": lambda x: truth(x.is_space),
-            "punct": lambda x: truth(x.is_punct),
-            "stop": lambda x: truth(x.is_stop),
+            "space": lambda x: operator.truth(x.is_space),
+            "punct": lambda x: operator.truth(x.is_punct),
+            "stop": lambda x: operator.truth(x.is_stop),
         }
         self.right_ignore_rules = {
-            "space": lambda x: truth(x.is_space),
-            "punct": lambda x: truth(x.is_punct),
-            "stop": lambda x: truth(x.is_stop),
+            "space": lambda x: operator.truth(x.is_space),
+            "punct": lambda x: operator.truth(x.is_punct),
+            "stop": lambda x: operator.truth(x.is_stop),
         }
 
-    def best_match(self, doc, query, step=1, flex=1, verbose=False):
+    def best_match(
+        self,
+        doc,
+        query,
+        fuzzy_alg=fuzz.ratio,
+        min_ratio=70,
+        case_sensitive=False,
+        step=1,
+        flex=1,
+        verbose=False,
+    ):
         query = self.nlp.make_doc(query)
         flex = self._calc_flex(flex, query)
-        match_values = self._scan_doc(doc, query, step, verbose)
+        match_values = self._scan_doc(
+            doc, query, fuzzy_alg, case_sensitive, step, verbose
+        )
         pos = self._index_max(match_values) * step
         match = self._adjust_left_right_positions(
-            doc, query, match_values, pos, step, flex, verbose
+            doc,
+            query,
+            match_values,
+            fuzzy_alg,
+            case_sensitive,
+            pos,
+            step,
+            flex,
+            verbose,
         )
-        if match[2] >= self.min_ratio:
+        if match[2] >= min_ratio:
             match = (doc[match[0] : match[1]], match[0], match[1], match[2])
             return match
 
-    def multi_match(self, doc, query, max_results=3, step=1, flex=1, verbose=False):
+    def multi_match(
+        self,
+        doc,
+        query,
+        max_results=3,
+        fuzzy_alg=fuzz.ratio,
+        min_ratio=70,
+        case_sensitive=False,
+        step=1,
+        flex=1,
+        verbose=False,
+    ):
         query = self.nlp.make_doc(query)
         flex = self._calc_flex(flex, query)
-        match_values = self._scan_doc(doc, query, step, verbose)
+        match_values = self._scan_doc(
+            doc, query, fuzzy_alg, case_sensitive, step, verbose
+        )
         positions = [
             pos * step for pos in self._indice_maxes(match_values, max_results)
         ]
         matches = [
             self._adjust_left_right_positions(
-                doc, query, match_values, pos, step, flex, verbose
+                doc,
+                query,
+                match_values,
+                fuzzy_alg,
+                case_sensitive,
+                pos,
+                step,
+                flex,
+                verbose,
             )
             for pos in positions
         ]
         matches = [
             (doc[match[0] : match[1]], match[0], match[1], match[2])
             for match in matches
-            if match[2] >= self.min_ratio
+            if match[2] >= min_ratio
         ]
-        matches = sorted(matches, key=itemgetter(3), reverse=True)
+        matches = sorted(matches, key=operator.itemgetter(3), reverse=True)
         matches = self._filter_overlapping_matches(matches)
         return matches
 
-    def match(self, a, b):
-        if not self.case_sensitive:
+    def match(self, a, b, fuzzy_alg, case_sensitive):
+        if not case_sensitive:
             a = a.lower()
             b = b.lower()
-        return self.fuzzy_alg(a, b)
+        return fuzzy_alg(a, b)
 
     def _calc_flex(self, flex, query):
         if flex >= len(query) / 2:
             flex = 1
         return flex
 
-    def _scan_doc(self, doc, query, step, verbose):
+    def _scan_doc(self, doc, query, fuzzy_alg, case_sensitive, step, verbose):
         match_values = []
         m = 0
         while m + len(query) - step <= len(doc) - 1:
-            match_values.append(self.match(query.text, doc[m : m + len(query)].text))
+            match_values.append(
+                self.match(
+                    query.text, doc[m : m + len(query)].text, fuzzy_alg, case_sensitive
+                )
+            )
             if verbose:
                 print(
                     query,
                     "-",
                     doc[m : m + len(query)],
-                    self.match(query.text, doc[m : m + len(query)].text),
+                    self.match(
+                        query.text,
+                        doc[m : m + len(query)].text,
+                        fuzzy_alg,
+                        case_sensitive,
+                    ),
                 )
             m += step
         return match_values
@@ -147,26 +190,43 @@ class FuzzySearch:
         )
 
     def _adjust_left_right_positions(
-        self, doc, query, match_values, pos, step, flex, verbose
+        self,
+        doc,
+        query,
+        match_values,
+        fuzzy_alg,
+        case_sensitive,
+        pos,
+        step,
+        flex,
+        verbose,
     ):
         p_l, bp_l = [pos] * 2
         p_r, bp_r = [pos + len(query)] * 2
         bmv_l = match_values[int(p_l / step)]
         bmv_r = match_values[int(p_l / step)]
         for f in range(flex):
-            ll = self.match(query.text, doc[p_l - f : p_r].text)
+            ll = self.match(
+                query.text, doc[p_l - f : p_r].text, fuzzy_alg, case_sensitive
+            )
             if ll > bmv_l:
                 bmv_l = ll
                 bp_l = p_l - f
-            lr = self.match(query.text, doc[p_l + f : p_r].text)
+            lr = self.match(
+                query.text, doc[p_l + f : p_r].text, fuzzy_alg, case_sensitive
+            )
             if lr > bmv_l:
                 bmv_l = lr
                 bp_l = p_l + f
-            rl = self.match(query.text, doc[p_l : p_r - f].text)
+            rl = self.match(
+                query.text, doc[p_l : p_r - f].text, fuzzy_alg, case_sensitive
+            )
             if rl > bmv_r:
                 bmv_r = rl
                 bp_r = p_r - f
-            rr = self.match(query.text, doc[p_l : p_r + f].text)
+            rr = self.match(
+                query.text, doc[p_l : p_r + f].text, fuzzy_alg, case_sensitive
+            )
             if rr > bmv_r:
                 bmv_r = rr
                 bp_r = p_r + f
@@ -179,7 +239,11 @@ class FuzzySearch:
                 print("rl: -- value: %f -- snippet: %s" % (rl, doc[p_l : p_r - f].text))
                 print("rr: -- value: %f -- snippet: %s" % (rl, doc[p_l : p_r + f].text))
             bp_l, bp_r = self._enforce_rules(doc, bp_l, bp_r)
-            return bp_l, bp_r, self.match(query.text, doc[bp_l:bp_r].text)
+            return (
+                bp_l,
+                bp_r,
+                self.match(query.text, doc[bp_l:bp_r].text, fuzzy_alg, case_sensitive),
+            )
 
     def _enforce_rules(self, doc, bp_l, bp_r):
         if self.left_ignores:
@@ -194,21 +258,21 @@ class FuzzySearch:
                 if bp_l == bp_r - 1:
                     break
                 bp_l += 1
+        if self.right_ignores:
             bp_r -= 1
-            if self.right_ignores:
-                right_ignore_funcs = []
-                for key in self.right_ignores:
-                    try:
-                        func = self.right_ignore_rules[key]
-                        right_ignore_funcs.append(func)
-                    except KeyError:
-                        pass
+            right_ignore_funcs = []
+            for key in self.right_ignores:
+                try:
+                    func = self.right_ignore_rules[key]
+                    right_ignore_funcs.append(func)
+                except KeyError:
+                    pass
             while any([func(doc[bp_r]) for func in right_ignore_funcs]):
                 if bp_r == bp_l:
                     break
                 bp_r -= 1
             bp_r += 1
-            return bp_l, bp_r
+        return bp_l, bp_r
 
     @staticmethod
     def _filter_overlapping_matches(matches):
--- annotacy/tests/test.py
+++ annotacy/tests/test.py
@@ -2,7 +2,7 @@ import spacy
 from annotacy.annotacy import FuzzySearch
 
 nlp = spacy.blank("en")
-text = nlp("The cow said moo I'm a cow.")
+text = nlp("The cow the said moo I'm a cow, I'm a cow, I'm a cow.")
 
 fs = FuzzySearch(nlp)
-print(fs.multi_match(text, "cow"))
+print(fs.best_match(text, "cow"))
