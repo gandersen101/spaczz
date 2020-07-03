0ca39d5a43dc00a488b3846ab1d88d92df930a30
gandersen101 <gandersen.codes@gmail.com>
2020-07-02 23:41:32 -0500

    Updating RegexMatcher testing.

--- tests/test_matcher/test_regexmatcher.py
+++ tests/test_matcher/test_regexmatcher.py
@@ -2,17 +2,11 @@
 from typing import List, Tuple
 
 import pytest
-import spacy
+from spacy.language import Language
 from spacy.tokens import Doc, Span
 
+from spaczz.exceptions import KwargsWarning
 from spaczz.matcher.regexmatcher import RegexMatcher
-
-# Global Variables
-nlp = spacy.blank("en")
-doc = nlp.make_doc("I live at 555 Fake Street in Generic, TN 55555.")
-rm = RegexMatcher(
-    nlp.vocab
-)  # This RegexMatcher instance will be updated through the testing.
 
 
 def add_gpe_ent(
@@ -24,137 +18,35 @@ def add_gpe_ent(
     doc.ents += (entity,)
 
 
-def test_add() -> None:
-    """It adds the "GPE" label and a pattern to the matcher."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add("GPE", ["[U](nited|\\.?) ?[S](tates|\\.?)"])
-    assert "GPE" in temp_rm
+@pytest.fixture
+def test_doc(nlp: Language) -> Doc:
+    """Doc for testing."""
+    return nlp.make_doc("I live at 555 Fake Street in Generic, TN 55555 in the usa.")
 
 
-def test_add_with_kwargs() -> None:
-    """It adds the "GPE" label and a pattern with some kwargs to the matcher."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add(
-        "GPE", ["[U](nited|\\.?) ?[S](tates|\\.?)"], kwargs=[{"ignore_case": True}]
-    )
-    assert temp_rm.patterns == [
+@pytest.fixture
+def matcher_with_patterns(nlp: Language) -> RegexMatcher:
+    """Regex matcher with patterns added."""
+    rm = RegexMatcher(nlp.vocab)
+    rm.add("GPE", ["(?i)[U](nited|\\.?) ?[S](tates|\\.?)"], on_match=add_gpe_ent)
+    rm.add("STREET", ["street_addresses"], kwargs=[{"predef": True}])
+    rm.add("ZIP", ["zip_codes"], kwargs=[{"predef": True}])
+    return rm
+
+
+def test_adding_patterns(matcher_with_patterns: RegexMatcher) -> None:
+    """Regex matcher with patterns added."""
+    assert matcher_with_patterns.patterns == [
         {
             "label": "GPE",
-            "pattern": "[U](nited|\\.?) ?[S](tates|\\.?)",
+            "pattern": "(?i)[U](nited|\\.?) ?[S](tates|\\.?)",
             "type": "regex",
-            "kwargs": {"ignore_case": True},
-        }
-    ]
-
-
-def test_add_with_more_patterns_than_explicit_kwargs_warns() -> None:
-    """It will warn when more patterns are added than explicit kwargs."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    with pytest.warns(UserWarning):
-        temp_rm.add("TEST", ["Test1", "Test2"], [{"ignore_case": True}])
-
-
-def test_add_with_more_explicit_kwargs_than_patterns_warns() -> None:
-    """It will warn when more explicit kwargs are added than patterns."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    with pytest.warns(UserWarning):
-        temp_rm.add("TEST", ["Test1"], [{"ignore_case": True}, {"ignore_case": True}])
-
-
-def test_add_without_string_pattern_raises_error() -> None:
-    """Trying to add non strings as patterns raises a TypeError."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    with pytest.raises(TypeError):
-        temp_rm.add("TEST", [nlp.make_doc("Test1")])
-
-
-def test_add_str_pattern_outside_list_raises_error() -> None:
-    """Trying to add string as patterns, not iterable of strings, raises a TypeError."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    with pytest.raises(TypeError):
-        temp_rm.add("TEST", "Test1")
-
-
-def test_add_where_kwargs_are_not_dicts_raises_error() -> None:
-    """Trying to add non Dict objects as kwargs raises a TypeError."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    with pytest.raises(TypeError):
-        temp_rm.add("TEST", ["Test1"], ["ignore_case"])
-
-
-def test_len_returns_count_of_labels_in_matcher() -> None:
-    """It returns the correct length of labels."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add("GPE", ["[U](nited|\\.?) ?[S](tates|\\.?)"])
-    assert len(temp_rm) == 1
-
-
-def test_in_returns_bool_of_label_in_matcher() -> None:
-    """It returns whether a label is present."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add("GPE", ["[U](nited|\\.?) ?[S](tates|\\.?)"])
-    assert "GPE" in temp_rm
-
-
-def test_labels_returns_label_names() -> None:
-    """It returns a tuple of all unique label names."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add("GPE", ["[U](nited|\\.?) ?[S](tates|\\.?)"])
-    assert temp_rm.labels == ("GPE",)
-
-
-def test_remove_label() -> None:
-    """It removes a label from the matcher."""
-    rm.add("TEST", ["test"])
-    assert "TEST" in rm
-    rm.remove("TEST")
-    assert "TEST" not in rm
-
-
-def test_remove_label_raises_error_if_label_not_in_matcher() -> None:
-    """It raises a ValueError if trying to remove a label not present."""
-    with pytest.raises(ValueError):
-        rm.remove("TEST")
-
-
-def test_matcher_returns_matches() -> None:
-    """Calling the matcher on a Doc object returns matches."""
-    rm.add("STREET", ["street_addresses"], [{"predef": True}])
-    rm.add("ZIP", ["zip_codes"], [{"predef": True}])
-    rm.add("GPE", ["TN", "Generic"])
-    assert rm(doc) == [("STREET", 3, 6), ("GPE", 7, 8), ("GPE", 9, 10), ("ZIP", 10, 11)]
-
-
-def test_matcher_returns_empty_list_if_no_matches() -> None:
-    """Calling the matcher on a Doc object with no viable matches returns empty list."""
-    temp_doc = nlp("No matches here.")
-    assert rm(temp_doc) == []
-
-
-def test_matcher_uses_on_match_callback() -> None:
-    """It utilizes callback on match functions passed when called on a Doc object."""
-    temp_doc = nlp.make_doc("I live in the united states, or the US.")
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add(
-        "GPE",
-        ["[U](nited|\\.?) ?[S](tates|\\.?)"],
-        kwargs=[{"ignore_case": True}],
-        on_match=add_gpe_ent,
-    )
-    temp_rm(temp_doc)
-    assert [ent.label_ for ent in temp_doc.ents] == ["GPE", "GPE"]
-
-
-def test_patterns_with_and_without_kwargs() -> None:
-    """It adds the "GPE" label and a pattern with some kwargs to the matcher."""
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add("GPE", ["[U](nited|\\.?) ?[S](tates|\\.?)"])
-    temp_rm.add("ZIP", ["zip_codes"], [{"predef": True}])
-    assert temp_rm.patterns == [
+        },
         {
-            "label": "GPE",
-            "pattern": "[U](nited|\\.?) ?[S](tates|\\.?)",
+            "label": "STREET",
+            "pattern": "street_addresses",
             "type": "regex",
+            "kwargs": {"predef": True},
         },
         {
             "label": "ZIP",
@@ -165,49 +57,171 @@ def test_patterns_with_and_without_kwargs() -> None:
     ]
 
 
-def test_matcher_pipe() -> None:
+# def test_add_with_kwargs() -> None:
+#     """It adds the "GPE" label and a pattern with some kwargs to the matcher."""
+#     temp_rm = RegexMatcher(nlp.vocab)
+#     temp_rm.add(
+#         "GPE", ["[U](nited|\\.?) ?[S](tates|\\.?)"], kwargs=[{"ignore_case": True}]
+#     )
+#     assert temp_rm.patterns == [
+#         {
+#             "label": "GPE",
+#             "pattern": "[U](nited|\\.?) ?[S](tates|\\.?)",
+#             "type": "regex",
+#             "kwargs": {"ignore_case": True},
+#         }
+#     ]
+
+
+def test_add_with_more_patterns_than_explicit_kwargs_warns(
+    matcher_with_patterns: RegexMatcher,
+) -> None:
+    """It will warn when more patterns are added than explicit kwargs."""
+    with pytest.warns(KwargsWarning):
+        matcher_with_patterns.add("TEST", ["Test1", "Test2"], [{"ignore_case": True}])
+
+
+def test_add_with_more_explicit_kwargs_than_patterns_warns(
+    matcher_with_patterns: RegexMatcher,
+) -> None:
+    """It will warn when more explicit kwargs are added than patterns."""
+    with pytest.warns(KwargsWarning):
+        matcher_with_patterns.add(
+            "TEST", ["Test1"], [{"ignore_case": True}, {"ignore_case": True}]
+        )
+
+
+def test_add_without_string_pattern_raises_error(
+    matcher_with_patterns: RegexMatcher, nlp: Language
+) -> None:
+    """Trying to add non strings as patterns raises a TypeError."""
+    with pytest.raises(TypeError):
+        matcher_with_patterns.add("TEST", [nlp.make_doc("Test1")])
+
+
+def test_add_str_pattern_outside_list_raises_error(
+    matcher_with_patterns: RegexMatcher,
+) -> None:
+    """Trying to add string as patterns, not iterable of strings, raises a TypeError."""
+    with pytest.raises(TypeError):
+        matcher_with_patterns.add("TEST", "Test1")
+
+
+def test_add_where_kwargs_are_not_dicts_raises_error(
+    matcher_with_patterns: RegexMatcher,
+) -> None:
+    """Trying to add non Dict objects as kwargs raises a TypeError."""
+    with pytest.raises(TypeError):
+        matcher_with_patterns.add("TEST", ["Test1"], ["ignore_case"])
+
+
+def test_len_returns_count_of_labels_in_matcher(
+    matcher_with_patterns: RegexMatcher,
+) -> None:
+    """It returns the correct length of labels."""
+    assert len(matcher_with_patterns) == 3
+
+
+def test_in_returns_bool_of_label_in_matcher(
+    matcher_with_patterns: RegexMatcher,
+) -> None:
+    """It returns whether a label is present."""
+    assert "GPE" in matcher_with_patterns
+
+
+def test_labels_returns_label_names(matcher_with_patterns: RegexMatcher) -> None:
+    """It returns a tuple of all unique label names."""
+    assert all(
+        label in matcher_with_patterns.labels for label in ("GPE", "ZIP", "STREET")
+    )
+
+
+def test_remove_label(matcher_with_patterns: RegexMatcher) -> None:
+    """It removes a label from the matcher."""
+    matcher_with_patterns.add("TEST", ["test"])
+    assert "TEST" in matcher_with_patterns
+    matcher_with_patterns.remove("TEST")
+    assert "TEST" not in matcher_with_patterns
+
+
+def test_remove_label_raises_error_if_label_not_in_matcher(
+    matcher_with_patterns: RegexMatcher,
+) -> None:
+    """It raises a ValueError if trying to remove a label not present."""
+    with pytest.raises(ValueError):
+        matcher_with_patterns.remove("TEST")
+
+
+def test_matcher_returns_matches(
+    matcher_with_patterns: RegexMatcher, test_doc: Doc
+) -> None:
+    """Calling the matcher on a Doc object returns matches."""
+    assert matcher_with_patterns(test_doc) == [
+        ("STREET", 3, 6),
+        ("ZIP", 10, 11),
+        ("GPE", 13, 14),
+    ]
+
+
+def test_matcher_returns_empty_list_if_no_matches(
+    matcher_with_patterns: RegexMatcher, nlp: Language
+) -> None:
+    """Calling the matcher on a Doc object with no viable matches returns empty list."""
+    doc = nlp("No matches here.")
+    assert matcher_with_patterns(doc) == []
+
+
+def test_matcher_uses_on_match_callback(
+    matcher_with_patterns: RegexMatcher, test_doc: Doc
+) -> None:
+    """It utilizes callback on match functions passed when called on a Doc object."""
+    matcher_with_patterns(test_doc)
+    assert "usa" in [ent.text for ent in test_doc.ents]
+
+
+def test_matcher_pipe(nlp: Language) -> None:
     """It returns a stream of Doc objects."""
     doc_stream = (
         nlp.make_doc("test doc 1: United States"),
         nlp.make_doc("test doc 2: US"),
     )
-    temp_rm = RegexMatcher(nlp.vocab)
-    output = temp_rm.pipe(doc_stream)
+    rm = RegexMatcher(nlp.vocab)
+    output = rm.pipe(doc_stream)
     assert list(output) == list(doc_stream)
 
 
-def test_matcher_pipe_with_context() -> None:
+def test_matcher_pipe_with_context(nlp: Language) -> None:
     """It returns a stream of Doc objects as tuples with context."""
     doc_stream = (
         (nlp.make_doc("test doc 1: United States"), "Country"),
         (nlp.make_doc("test doc 2: US"), "Country"),
     )
-    temp_rm = RegexMatcher(nlp.vocab)
-    output = temp_rm.pipe(doc_stream, as_tuples=True)
+    rm = RegexMatcher(nlp.vocab)
+    output = rm.pipe(doc_stream, as_tuples=True)
     assert list(output) == list(doc_stream)
 
 
-def test_matcher_pipe_with_matches() -> None:
+def test_matcher_pipe_with_matches(nlp: Language) -> None:
     """It returns a stream of Doc objects and matches as tuples."""
     doc_stream = (
         nlp.make_doc("test doc 1: United States"),
         nlp.make_doc("test doc 2: US"),
     )
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add("GPE", ["[Uu](nited|\\.?) ?[Ss](tates|\\.?)"])
-    output = temp_rm.pipe(doc_stream, return_matches=True)
+    rm = RegexMatcher(nlp.vocab)
+    rm.add("GPE", ["[Uu](nited|\\.?) ?[Ss](tates|\\.?)"])
+    output = rm.pipe(doc_stream, return_matches=True)
     matches = [entry[1] for entry in output]
     assert matches == [[("GPE", 4, 6)], [("GPE", 4, 5)]]
 
 
-def test_matcher_pipe_with_matches_and_context() -> None:
+def test_matcher_pipe_with_matches_and_context(nlp: Language) -> None:
     """It returns a stream of Doc objects, matches, and context as a tuple."""
     doc_stream = (
         (nlp.make_doc("test doc 1: United States"), "Country"),
         (nlp.make_doc("test doc 2: US"), "Country"),
     )
-    temp_rm = RegexMatcher(nlp.vocab)
-    temp_rm.add("GPE", ["[Uu](nited|\\.?) ?[Ss](tates|\\.?)"])
-    output = temp_rm.pipe(doc_stream, return_matches=True, as_tuples=True)
+    rm = RegexMatcher(nlp.vocab)
+    rm.add("GPE", ["[Uu](nited|\\.?) ?[Ss](tates|\\.?)"])
+    output = rm.pipe(doc_stream, return_matches=True, as_tuples=True)
     matches = [(entry[0][1], entry[1]) for entry in output]
     assert matches == [([("GPE", 4, 6)], "Country"), ([("GPE", 4, 5)], "Country")]
