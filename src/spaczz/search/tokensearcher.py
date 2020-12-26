138ea08f8d92f4e8309efd6cf0965832579ece01
gandersen101 <gandersen.codes@gmail.com>
2020-12-26 14:42:42 -0600

    Tests for tokensearcher.

--- src/spaczz/search/tokensearcher.py
+++ src/spaczz/search/tokensearcher.py
@@ -17,13 +17,27 @@ class TokenSearcher:
         self._fuzzy_funcs: FuzzyFuncs = FuzzyFuncs()
 
     def fuzzy_compare(
-        self, a: str, b: str, ignore_case: bool = True, fuzzy_func: str = "simple",
+        self,
+        doc_text: str,
+        pattern: str,
+        ignore_case: bool,
+        fuzzy_func: str = "simple",
     ) -> int:
         """Pass."""
         if ignore_case:
-            a = a.lower()
-            b = b.lower()
-        return round(self._fuzzy_funcs.get(fuzzy_func)(a, b))
+            doc_text = doc_text.lower()
+            pattern = pattern.lower()
+        return round(self._fuzzy_funcs.get(fuzzy_func)(doc_text, pattern))
+
+    @staticmethod
+    def regex_compare(doc_text: str, pattern: str, ignore_case: bool) -> bool:
+        """Pass."""
+        if ignore_case:
+            doc_text = doc_text.lower()
+        if regex.match(pattern, doc_text):
+            return True
+        else:
+            return False
 
     def match(
         self,
@@ -49,17 +63,16 @@ class TokenSearcher:
     @staticmethod
     def _parse_case(token: Dict[str, Any]) -> Tuple[Union[str, dict, None], str, bool]:
         """Pass."""
-        cased = token.get("TEXT")
-        if cased:
-            return cased, "TEXT", False
+        if token.get("TEXT"):
+            return token.get("TEXT"), "TEXT", False
         else:
             return token.get("LOWER"), "LOWER", True
 
     @staticmethod
-    def _parse_type(text: Dict[str, Any]) -> Tuple[str, str]:
+    def _parse_type(pattern_dict: Dict[str, Any]) -> Tuple[str, str]:
         """Pass."""
-        fuzzy_text = text.get("FUZZY")
-        regex_text = text.get("FREGEX")
+        fuzzy_text = pattern_dict.get("FUZZY")
+        regex_text = pattern_dict.get("FREGEX")
         if isinstance(fuzzy_text, str):
             return fuzzy_text, "FUZZY"
         elif isinstance(regex_text, str):
@@ -77,21 +90,21 @@ class TokenSearcher:
         """Pass."""
         seq_matches: List[Optional[Tuple[str, str]]] = []
         for i, token in enumerate(pattern):
-            text, case, case_bool = self._parse_case(token)
-            if isinstance(text, dict):
-                match_text, match_type = self._parse_type(text)
-                if match_text and match_type == "FUZZY":
+            pattern_dict, case, case_bool = self._parse_case(token)
+            if isinstance(pattern_dict, dict):
+                pattern_text, pattern_type = self._parse_type(pattern_dict)
+                if pattern_text and pattern_type == "FUZZY":
                     if self.fuzzy_compare(
                         seq[i].text,
-                        match_text,
+                        pattern_text,
                         case_bool,
-                        text.get("FUZZY_FUNC", fuzzy_func),
-                    ) >= text.get("MIN_R", min_r):
+                        pattern_dict.get("FUZZY_FUNC", fuzzy_func),
+                    ) >= pattern_dict.get("MIN_R", min_r):
                         seq_matches.append((case, seq[i].text))
                     else:
                         return []
-                elif match_text and match_type == "FREGEX":
-                    if regex.match(match_text, seq[i].text):
+                elif pattern_text and pattern_type == "FREGEX":
+                    if self.regex_compare(seq[i].text, pattern_text, case_bool):
                         seq_matches.append((case, seq[i].text))
                     else:
                         return []
--- tests/test_search/test_tokensearcher.py
+++ tests/test_search/test_tokensearcher.py
@@ -1,18 +1,50 @@
-"""Pass."""
-import spacy
+"""Tests for tokensearcher module."""
+import pytest
+from spacy.language import Language
+from spacy.tokens import Doc
 
-from spaczz.search.tokensearcher import TokenSearcher
+from spaczz.search import TokenSearcher
 
-nlp = spacy.blank("en")
-doc = nlp("The manager gave me databese acess so now I can acces the database.")
-ts = TokenSearcher(nlp.vocab)
-print(
-    ts.match(
-        doc,
-        [
-            {"TEXT": {"FREGEX": "(database){e<=1}"}},
-            {"LOWER": {"FUZZY": "access"}, "POS": "NOUN"},
-            {"TEXT": "the"},
-        ],
+
+@pytest.fixture
+def searcher(nlp: Language) -> TokenSearcher:
+    """It returns a token searcher."""
+    return TokenSearcher(vocab=nlp.vocab)
+
+
+@pytest.fixture
+def example(nlp: Language) -> Doc:
+    """Example doc for search."""
+    return nlp(
+        "The manager gave me SQL databesE ACESS so now I can acces the SQL databasE."
     )
-)
+
+
+def test_match_lower(searcher: TokenSearcher, example: Doc) -> None:
+    """The searcher with lower-cased text is working as intended."""
+    assert searcher.match(
+        example,
+        [
+            {"TEXT": "SQL"},
+            {"LOWER": {"FREGEX": "(database){e<=1}"}},
+            {"LOWER": {"FUZZY": "access"}, "POS": "NOUN"},
+        ],
+    ) == [[None, ("LOWER", "databesE"), ("LOWER", "ACESS")]]
+
+
+def test_match_text(searcher: TokenSearcher, example: Doc) -> None:
+    """The searcher with verbatim text is working as intended."""
+    assert searcher.match(
+        example,
+        [
+            {"TEXT": {"FUZZY": "access"}, "POS": "NOUN"},
+            {},
+            {"TEXT": {"REGEX": "[Ss][Qq][Ll]"}},
+            {"TEXT": {"FREGEX": "(database){e<=1}"}},
+        ],
+    ) == [[("TEXT", "acces"), None, None, ("TEXT", "databasE.")]]
+
+
+def test_no_matches(searcher: TokenSearcher, example: Doc) -> None:
+    """No matches returns empty list."""
+    assert searcher.match(example, [{"TEXT": {"FUZZY": "MongoDB"}}]) == []
