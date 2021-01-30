69baefdecca34b99578110c2f06e860fb20a073f
Bruno <brunobg@gmail.com>
2021-01-30 18:17:57 -0300

    doc

--- examples/countries.py
+++ examples/countries.py
@@ -52,6 +52,7 @@ def testSimilar():
         print(countries)
 
 def testLoremIpsum():
+    # this is a randomly generated text without any country names. 
     lorem = """
 One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. Before nature his parish boy. 
 
